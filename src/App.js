bbcbd8db8ea1418feeca13c3412d4737e469cef0
wangxu <1119720664@qq.com>
2019-02-22 16:30:02 +0800

    react-jianshu还原

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import { GlobalStyle } from './common/css/reset';
+import Header from "./components/header/header"
+import Index from "./components/index/index"
+import Detail from "./components/detail/detail"
+import Login from "./components/login/login"
+import Write from "./components/auth/auth"
+import { BrowserRouter, Route ,Switch} from "react-router-dom"
+import { Provider } from "react-redux"
+import store from "./store/store"
 
+/*异步加载组件的使用*/
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Fragment>
+                <GlobalStyle/>
+                <Provider store={store}>
+                    <Fragment>
+                        <BrowserRouter>
+                            <div>
+                                <Header/>
+                                <Switch>
+                                    <Route exact path="/" component={Index}/>
+                                    <Route exact path="/login" component={Login}/>
+                                    <Route exact path="/write" component={Write}/>
+                                    <Route exact path="/detail/:id" component={Detail}/>
+                                </Switch>
+                            </div>
+                        </BrowserRouter>
+                    </Fragment>
+                </Provider>
+            </Fragment>
+        );
+    }
 }
 
 export default App;
